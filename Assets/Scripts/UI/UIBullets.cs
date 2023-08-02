93eddd02c3580006b35777f099f9192407e67915
CharlieTheIndieDev <74312830+DoesDevStuff@users.noreply.github.com>
2023-08-02 13:35:36 +0100

    update ammo values in UI

--- Assets/Scripts/UI/UIBullets.cs
+++ Assets/Scripts/UI/UIBullets.cs
@@ -7,6 +7,8 @@ public class UIBullets : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI _text = null;
+    [SerializeField]
+    private TextMeshProUGUI _flaretext = null;
 
     public void UpdateBulletsText(int bulletCount)
     {
@@ -19,5 +21,18 @@ public class UIBullets : MonoBehaviour
             _text.color = Color.white;
         }
         _text.SetText(bulletCount.ToString());
+    }
+
+    public void UpdateFlareText(int flareCount)
+    {
+        if (flareCount == 0)
+        {
+            _flaretext.color = Color.red;
+        }
+        else
+        {
+            _flaretext.color = Color.white;
+        }
+        _flaretext.SetText(flareCount.ToString());
     }
 }
